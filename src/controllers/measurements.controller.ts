fa93ef1de7adeee11a076f8b90c104af964b3d8f
Vladislav Korniushenko <vkornushenko@gmail.com>
2026-06-14 18:35:12 +0300

    extra dev log disabled

--- src/controllers/measurements.controller.ts
+++ src/controllers/measurements.controller.ts
@@ -34,13 +34,22 @@ export async function getMeasurements(req: Request, res: Response) {
   const perPage = +(req.query.perPage || 25);
   const start = req.query.start ? new Date(String(req.query.start)) : null;
   const end = req.query.end ? new Date(String(req.query.end)) : null;
-  const sort = req.query.sort as SortOrder || 'desc';
+  const sort = (req.query.sort as SortOrder) || 'desc';
 
-  console.log(`req.query.page=${req.query.page}`)
-  console.log(`currentPage=${currentPage}, perPage=${perPage}`);
+  // console.log(`req.query.page=${req.query.page}`);
+  // console.log(`currentPage=${currentPage}, perPage=${perPage}`);
+  // console.log(`req.query.start=${req.query.start}`);
+  // console.log(`req.query.end=${req.query.end}`);
 
-  const {measurements, totalMeasurements} = await getMeasurementsService(req.user!.id, currentPage, perPage, start, end, sort);
-  console.log('getting Measurements')
+  const { measurements, totalMeasurements } = await getMeasurementsService(
+    req.user!.id,
+    currentPage,
+    perPage,
+    start,
+    end,
+    sort,
+  );
+  console.log('getting Measurements');
   res.status(200).json({
     data: measurements.map((m) => m.toObject()),
     totalMeasurements: totalMeasurements,
@@ -58,7 +67,7 @@ export async function getMeasurementById(req: Request, res: Response) {
     userId,
     measurementId,
   });
-  console.log('getting Measurement by ID')
+  console.log('getting Measurement by ID');
   res.status(200).json({
     data: measurement.toObject(),
   });
@@ -68,7 +77,7 @@ export async function deleteMeasurementById(req: Request, res: Response) {
   if (!req.user!.id) {
     throw new AppError('User not found', 404);
   }
-  
+
   const measurementId = req.params.id as string;
 
   const measurement = await deleteMeasurementByIdService({ measurementId });
